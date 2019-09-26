6c4db2d0cee47371a345a976b4b48f7ca886b18c
Luke Parker <luke@Lukes-MacBook-Pro.local>
2019-09-26 16:57:43 -0700

    armor class

--- superhero.py
+++ superhero.py
@@ -10,7 +10,16 @@ class Ability:
         ''' Return a value between 0 and the 
         value set by self.max_damage.'''
         return random.randint(0, self.max_damage)  
-        
+
+class Armor:
+    def __init__(self, name, max_block):
+        self.name = name
+        self.max_block = block
+    def block(self):
+        ''' Return a value between 0 and the 
+        value set by self.max_damage.'''
+        return random.randint(0, self.max_block)  
+
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
